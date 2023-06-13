a26f395d36fd00a1952ebe202bcd0e2460825305
AjayaBudha <ajaya.budhathoki@hotmail.com>
2023-06-13 12:05:06 +0100

    remove comment in Router

--- routes/postsRouter.js
+++ routes/postsRouter.js
@@ -2,7 +2,6 @@ const { Router } = require("express");
 const postController = require("../controllers/postController");
 
 const router = Router();
-// const postRouter = router;
 
 router
   .route("/")
