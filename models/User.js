3354aae42f4c7c6d19cb5842c737c39c7562b6d8
Chey-DB <cheydb@rocketmail.com>
2023-06-16 16:41:49 +0100

    Updated userSchema so more than one person can login

--- models/User.js
+++ models/User.js
@@ -12,78 +12,145 @@ const User = new UserSchema({
     username: {
       type: String,
       lowercase: true,
-      validate: {
-        validator: function() {
-          return this.method === 'local' ? this.local.username !== '' : true;
-        },
-        message: 'Username required for local method'
-      }
     },
     password: {
       type: String,
-      validate: {
-        validator: function() {
-          return this.method === 'local' ? this.local.password !== '' : true;
-        },
-        message: 'Password required'
-      }
     },
     profilePic: {
-          type: String,
-          required: false,
-        },
-        dueDate: {
-          type: Date,
-          required: false,
-        },
-        currentWeek: {
-          type: Number,
-          required: false,
-        },
-        about: {
-          type: String,
-          required: false,
-        }
+      type: String,
+      required: false,
+    },
+    dueDate: {
+      type: Date,
+      required: false,
+    },
+    currentWeek: {
+      type: Number,
+      required: false,
+    },
+    about: {
+      type: String,
+      required: false,
+    }
   },
   google: {
     id: {
       type: String,
-      validate: {
-        validator: function() {
-          return this.method === 'google' ? this.google.id !== '' : true;
-        },
-        message: 'Google id required'
-      }
     },
     username: {
       type: String,
       lowercase: true,
-      validate: {
-        validator: function() {
-          return this.method === 'google' ? this.google.givenName !== '' : true;
-        },
-        message: 'Username required for google method'
-      }
     },
     profilePic: {
-          type: String,
-          required: false,
-        },
-        dueDate: {
-          type: Date,
-          required: false,
-        },
-        currentWeek: {
-          type: Number,
-          required: false,
-        },
-        about: {
-          type: String,
-          required: false,
-        }
+      type: String,
+      required: false,
+    },
+    dueDate: {
+      type: Date,
+      required: false,
+    },
+    currentWeek: {
+      type: Number,
+      required: false,
+    },
+    about: {
+      type: String,
+      required: false,
+    }
   }
 });
 
+User.pre('save', function(next) {
+  if (this.method === 'google') {
+    this.local = undefined;
+  } else if (this.method === 'local') {
+    this.google = undefined;
+  }
+  next();
+});
 
 module.exports = mongoose.model("User", User);
+
+
+// const User = new UserSchema({
+//   method: {
+//     type: String,
+//     enum: ['local', 'google'],
+//     required: true
+//   },
+//   local: {
+//     username: {
+//       type: String,
+//       lowercase: true,
+//       validate: {
+//         validator: function() {
+//           return this.method === 'local' ? this.local.username !== '' : true;
+//         },
+//         message: 'Username required for local method'
+//       }
+//     },
+//     password: {
+//       type: String,
+//       validate: {
+//         validator: function() {
+//           return this.method === 'local' ? this.local.password !== '' : true;
+//         },
+//         message: 'Password required'
+//       }
+//     },
+//     profilePic: {
+//           type: String,
+//           required: false,
+//         },
+//         dueDate: {
+//           type: Date,
+//           required: false,
+//         },
+//         currentWeek: {
+//           type: Number,
+//           required: false,
+//         },
+//         about: {
+//           type: String,
+//           required: false,
+//         }
+//   },
+//   google: {
+//     id: {
+//       type: String,
+//       validate: {
+//         validator: function() {
+//           return this.method === 'google' ? this.google.id !== '' : true;
+//         },
+//         message: 'Google id required'
+//       }
+//     },
+//     username: {
+//       type: String,
+//       lowercase: true,
+//       validate: {
+//         validator: function() {
+//           return this.method === 'google' ? this.google.givenName !== '' : true;
+//         },
+//         message: 'Username required for google method'
+//       }
+//     },
+//     profilePic: {
+//           type: String,
+//           required: false,
+//         },
+//         dueDate: {
+//           type: Date,
+//           required: false,
+//         },
+//         currentWeek: {
+//           type: Number,
+//           required: false,
+//         },
+//         about: {
+//           type: String,
+//           required: false,
+//         }
+//   }
+// });
 
