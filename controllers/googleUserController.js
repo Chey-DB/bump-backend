0f48046ac6f263c18d7ad293d76a2d2253ffdd75
AjayaBudha <ajaya.budhathoki@hotmail.com>
2023-06-22 10:57:26 +0100

    added new routes/ updated routes on localUser/googleUser

--- controllers/googleUserController.js
+++ controllers/googleUserController.js
@@ -1,36 +1,44 @@
-const User = require("../models/GoogleUser");
+const GoogleUser = require("../models/GoogleUser");
 
 const index = async (req, res) => {
-    const googleUsers = await GoogleUser.find({});
-    res.json(googleUsers);
-    }
+  const googleUsers = await GoogleUser.find({});
+  res.json(googleUsers);
+};
 
 const show = async (req, res) => {
-    const googleUser = await GoogleUser.findById(req.params.id);
-    res.json(googleUser);
-    }
+  const googleUser = await GoogleUser.findById(req.params.id);
+  res.json(googleUser);
+};
 
 const create = async (req, res) => {
-    const googleUser = await GoogleUser.create(req.body);
-    res.json(googleUser);
-    }
+  const googleUser = await GoogleUser.create(req.body);
+  res.json(googleUser);
+};
 
 const update = async (req, res) => {
-    const googleUser = await GoogleUser.findById(req.params.id);
-    Object.assign(googleUser, req.body);
-    await googleUser.save();
-    res.json(googleUser);
-    }
+  const { id } = req.params;
+  const updateData = req.body;
+
+  try {
+    const updateUser = await GoogleUser.findOneAndUpdate(
+      { _id: id },
+      updateData
+    );
+    res.status(200).json(updateUser);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
 
 const destroy = async (req, res) => {
-    const googleUser = await GoogleUser.findByIdAndDelete(req.params.id);
-    res.json(googleUser);
-    }
+  const googleUser = await GoogleUser.findByIdAndDelete(req.params.id);
+  res.json(googleUser);
+};
 
 module.exports = {
-    index,
-    show,
-    create,
-    update,
-    destroy
-}
+  index,
+  show,
+  create,
+  update,
+  destroy,
+};
--- controllers/localUserController.js
+++ controllers/localUserController.js
@@ -6,26 +6,26 @@ const register = async (req, res) => {
   console.log(username, password);
   const localUser = await LocalUser.findOne({ username: req.body.username });
   if (localUser) {
-    res
-      .status(400)
-      .json({
-        message: { msgBody: "Username is already taken", msgError: true },
-      });
+    res.status(400).json({
+      message: { msgBody: "Username is already taken", msgError: true },
+    });
   } else {
-    const hashedPassword = await bcrypt.hash(password, Number(process.env.BCRYPT_SALT_ROUNDS));
+    const hashedPassword = await bcrypt.hash(
+      password,
+      Number(process.env.BCRYPT_SALT_ROUNDS)
+    );
 
     const newLocalUser = await LocalUser({
       username,
       password: hashedPassword,
+      profilePic: "",
     });
     try {
       const createLocalUser = await LocalUser.create(newLocalUser);
-      res
-        .status(201)
-        .json({
-          message: { msgBody: "Account successfully created", msgError: false },
-          localUser: createLocalUser,
-        });
+      res.status(201).json({
+        message: { msgBody: "Account successfully created", msgError: false },
+        localUser: createLocalUser,
+      });
     } catch (err) {
       res
         .status(500)
@@ -33,5 +33,15 @@ const register = async (req, res) => {
     }
   }
 };
+const getById = async (req, res) => {
+  const { id } = req.params;
 
-module.exports = { register };
+  try {
+    const response = await LocalUser.findById(id);
+
+    res.status(200).json(response);
+  } catch (error) {
+    res.status(404).json({ error: "Not Found", message: "User not found" });
+  }
+};
+module.exports = { register, getById };
