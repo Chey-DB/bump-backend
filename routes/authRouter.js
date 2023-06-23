77f5d74dbbd4d2d6e9787228bb78845f48b6626f
AjayaBudha <ajaya.budhathoki@hotmail.com>
2023-06-23 10:38:00 +0100

    change localhost to render

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -2,7 +2,6 @@ const Settings = require("../models/Settings");
 
 const passport = require("passport");
 
-
 const router = require("express").Router();
 
 const authRouter = router;
@@ -16,15 +15,14 @@ authRouter.get(
 authRouter.get(
   "/google/callback",
   passport.authenticate("google", {
-    failureRedirect: "http://localhost:5173/register",
+    failureRedirect: "https://bump-kwkn.onrender.com/register",
   }),
   async (req, res) => {
     console.log("You have successfully logged in!");
     const userSettings = await Settings.findOne({ user_id: req.user._id });
     if (userSettings) {
-      res.redirect("http://localhost:5173/dashboard");
-    } else
-    res.redirect("http://localhost:5173/settings");
+      res.redirect("https://bump-kwkn.onrender.com/dashboard");
+    } else res.redirect("https://bump-kwkn.onrender.com/settings");
   }
 );
 
@@ -43,14 +41,14 @@ authRouter.get("/logout", (req, res) => {
 
 authRouter.get("/checkUser", (req, res) => {
   console.log(req.user);
-  const user = req.session.passport.user
+  const user = req.session.passport.user;
   console.log(user);
   if (req.user) {
     res.send(req.user);
-    } else {
-      console.log("No user found at backend");
-      res.send("No user found");
-    }
+  } else {
+    console.log("No user found at backend");
+    res.send("No user found");
+  }
 });
 
 // authRouter.get('/checkUser', (req, res) => {
@@ -62,6 +60,5 @@ authRouter.get("/checkUser", (req, res) => {
 //     res.send({ loggedIn: false });
 //   }
 // });
-
 
 module.exports = authRouter;
